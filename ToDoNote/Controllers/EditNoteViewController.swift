89adec589d94f149c036ec0503f71fc9447fad39
Vincent Garde <ewilan@Vincents-iMac.local>
2018-11-30 09:47:41 +0100

    Filled note creation view + date picker OK

--- ToDoNote/Controllers/EditNoteViewController.swift
+++ ToDoNote/Controllers/EditNoteViewController.swift
@@ -12,12 +12,43 @@ class EditNoteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let toolBar = UIToolbar().ToolbarPiker(mySelect: #selector(EditNoteViewController.dismissPicker))
+        dateTextField.inputAccessoryView = toolBar
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var titleInput: UITextField!
+    @IBOutlet weak var detailInput: UITextField!
+    
+    
+    @objc func dismissPicker() {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func deleteNote(_ sender: Any) {
+    }
+    
+    @IBAction func saveNote(_ sender: Any) {
+    }
+    
+    @IBAction func textFieldEditing(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePicker.Mode.date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(EditNoteViewController.datePickerValueChanged), for: UIControl.Event.valueChanged)
+    }
+    
+    @objc func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        dateTextField.text = dateFormatter.string(from: sender.date)
+    }
+    
+   /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
